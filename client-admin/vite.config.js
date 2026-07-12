5840d8ad27e93f1e2febfb386254b7622c075a95
elopez-2021393 <elopez-2021393@kinal.edu.gt>
2026-07-12 11:34:06 -0600

    feat: mejorar config de workbox para PWA (globPatterns + navigateFallback)

--- client-admin/vite.config.js
+++ client-admin/vite.config.js
@@ -42,6 +42,8 @@ export default defineConfig({
         ],
       },
       workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,woff2}'],
+        navigateFallback: '/index.html',
         navigateFallbackDenylist: [/^\/api/],
       },
     }),
